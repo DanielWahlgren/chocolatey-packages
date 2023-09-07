382d9fab3377617e5dc59be68c5ef5ebc6410957
Daniel Wahlgren <daniel.wahlgren@gmail.com>
2023-09-07 10:31:25 +0200

    [PUSH cosafe safeexambrowser]

--- cosafe/update.ps1
+++ cosafe/update.ps1
@@ -4,7 +4,7 @@ $releases = 'https://support.cosafe.se/sv/articles/7638-cosafe-warning-system-cw
 
 
 function global:au_SearchReplace {
-   @{
+    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
@@ -15,7 +15,7 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
-    $url = $download_page.Links | ? href -match '.exe$' | % href | select -First 1
+    $url = $download_page.Links | Where-Object href -match '.exe$' | ForEach-Object href | Select-Object -First 1
     $version = ($url -split '-').Replace('.exe','')[-1]
 
     @{
