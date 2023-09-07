0ea3064e3b945adf38084167602a5496de78292f
Daniel Wahlgren <daniel.wahlgren@gmail.com>
2023-09-07 16:20:16 +0200

    [AU cosafe safeexambrowser]

--- safeexambrowser/update.ps1
+++ safeexambrowser/update.ps1
@@ -4,12 +4,12 @@ $releases = 'https://github.com/SafeExamBrowser/seb-win-refactoring/releases'
 
 
 function global:au_SearchReplace {
-   @{
+    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*url64\s*=\s*)('.*')"        = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -22,7 +22,7 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri ($releases + '/expanded_assets/' + $version)
     $url64 = $download_page.Links | ? href -match '.x64_Setup.msi$' | % href | select -First 1
     $url32 = $download_page.Links | ? href -match '.x86_Setup.msi$' | % href | select -First 1
-    $version = (Split-Path ( Split-Path $url ) -Leaf).Substring(1)
+    $version = (Split-Path ( Split-Path $url32 ) -Leaf).Substring(1)
 
     @{
         URL32   = 'https://github.com' + $url32
@@ -31,4 +31,4 @@ function global:au_GetLatest {
     }
 }
 
-update -ChecksumFor 32
+update
