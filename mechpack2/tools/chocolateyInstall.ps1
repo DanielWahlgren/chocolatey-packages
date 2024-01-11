45d6d1f3ab317b623e3d1916f475a84ab03074a2
Daniel Wahlgren <daniel.wahlgren@gmail.com>
2024-01-11 11:17:43 +0100

    Initial mechpack2

---
+++ mechpack2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿#https://safeexambrowser.org/about_overview_en.html
+
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName            = 'mechpack2'
+  fileType               = 'exe'
+  url                    = 'https://www.microtechstelladata.se/download/1859/?tmstv=1704965288'
+  checksum               = '17d7c714c2a6bdee0d2e09875eee24274448668c03ff453d3141ca635eb6353a'
+  checksumType           = 'sha256'
+  silentArgs             = '/verysilent /norestart'
+  validExitCodes         = @(0)
+  softwareName           = 'MechPack2*'
+}
+Install-ChocolateyPackage @packageArgs
