33e62799f0b09cb390ac7436489cba76d9aec708
Daniel Wahlgren <daniel.wahlgren@gmail.com>
2024-01-30 09:23:15 +0100

    [PUSH mechpack1]

--- mechpack1/tools/chocolateyInstall.ps1
+++ mechpack1/tools/chocolateyInstall.ps1
@@ -3,13 +3,13 @@
 $ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName            = 'mechpack2'
+  packageName            = 'mechpack1'
   fileType               = 'exe'
-  url                    = 'https://www.microtechstelladata.se/download/1859/?tmstv=1704965288'
-  checksum               = '17d7c714c2a6bdee0d2e09875eee24274448668c03ff453d3141ca635eb6353a'
+  url                    = ''
+  checksum               = ''
   checksumType           = 'sha256'
   silentArgs             = '/verysilent /norestart'
   validExitCodes         = @(0)
-  softwareName           = 'MechPack2*'
+  softwareName           = 'MechPack1*'
 }
 Install-ChocolateyPackage @packageArgs
