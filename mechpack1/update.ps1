1d8515ebff66d0a47756270dc3caed5a16479b28
Daniel Wahlgren <daniel.wahlgren@gmail.com>
2024-01-30 09:15:20 +0100

    Update MechPack2 and new MechPack1

---
+++ mechpack1/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$releases = 'https://www.microtechstelladata.se/download/1938/?tmstv=1706602274'
+
+
+function global:au_SearchReplace {
+    @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases
+
+    $link = $download_page.Links | ? href -like '*/download/*' | % href | select -First 1
+    $dlversion = ($link -split '\?')[0] | Select-string "(\d+)" | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
+    $version = "2.0.0.$dlversion"
+    $url32 = $link
+
+    @{
+        URL32   = $url32
+        Version = $version
+    }
+}
+
+update
