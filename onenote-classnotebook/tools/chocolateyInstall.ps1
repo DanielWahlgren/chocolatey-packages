9e4ccbb641baeae1fa0ffc9fccc7bfca9d13b007
Daniel Wahlgren <daniel.wahlgren@gmail.com>
2024-07-23 12:56:23 +0200

    Dependency + silentargs [PUSH onenote-classnotebook]

--- onenote-classnotebook/tools/chocolateyInstall.ps1
+++ onenote-classnotebook/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   url                    = 'https://download.microsoft.com/download/A/1/7/A171D312-CBAD-4E00-819B-0F56956DAC88/ClassNotebook.msi'
   checksum               = '1faf77b35845fffb99a8193ba78d4ef123b00f8305704bc3acd75178ed5eee5c'
   checksumType           = 'sha256'
-  silentArgs             = '/qn'
+  silentArgs             = '/quiet /norestart'
   validExitCodes         = @(0)
   softwareName           = 'OneNote Class Notebook*'
 }
