c2003c8e3be07c9a844ceb260c66f529638ab6a8
Daniel Wahlgren <daniel.wahlgren@gmail.com>
2024-07-24 09:39:24 +0200

    New silentArgs [PUSH onenote-classnotebook]

--- onenote-classnotebook/tools/chocolateyInstall.ps1
+++ onenote-classnotebook/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
-﻿#https://safeexambrowser.org/about_overview_en.html
+﻿
 
 $ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName            = 'onenote.classnotebook'
+  packageName            = 'onenote-classnotebook'
   fileType               = 'msi'
   url                    = 'https://download.microsoft.com/download/A/1/7/A171D312-CBAD-4E00-819B-0F56956DAC88/ClassNotebook.msi'
   checksum               = '1faf77b35845fffb99a8193ba78d4ef123b00f8305704bc3acd75178ed5eee5c'
   checksumType           = 'sha256'
-  silentArgs             = '/quiet /norestart'
+  silentArgs             = '/quiet /norestart REINSTALL=`"ALL`" REINSTALLMODE=`"vomus`" REBOOT=`"ReallySuppress`" UILevel=`"2`"'
   validExitCodes         = @(0)
   softwareName           = 'OneNote Class Notebook*'
 }
